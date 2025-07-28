469fb251149a4d8e7af415fc9c9c25a5a9a4edc6
Basile <basile.fuchs@chu-brest.fr>
2025-07-28 19:41:39 +0200

    feat(modules): add decription

--- clinical_situation/modules/__init__.py
+++ clinical_situation/modules/__init__.py
@@ -15,18 +15,33 @@ class ExtractDiagnosis(dspy.Signature):
     text: str = dspy.InputField(desc="un compte rendu médical.")
     entities: list[str] = dspy.OutputField(desc="la pathologie principale.")
 
+
 class ExtractCares(dspy.Signature):
     """Extraire tous les soins d'un compte rendu médicale."""
 
     text: str = dspy.InputField(desc="un compte rendu médical.")
     entities: list[str] = dspy.OutputField(desc="une liste de tous les soins.")
 
-class Classify(dspy.Signature):
-    """Classifier la situation clinique d'une hopitalisation à partir d'un compte rendu médical."""
 
-    text: str = dspy.InputField(desc="un compte rendu médical.")
-    classification: Literal["Diagnostic", "Traitement", "Surveillance"] = dspy.OutputField()
+class Classify(dspy.Signature):
+    """
+    Classer la situation clinique d'une hopitalisation à partir d'un text enrichi.
+    Classification possible :
+      - Diagnostic : La situation est celle d’un patient hospitalisé en raison d’une symptomatologie,
+      pour un diagnostic étiologique. Le mot symptomatologie inclut les signes cliniques et les résultats
+      anormaux d’examens complémentaires.
+      - Traitement : La situation est celle d’un patient atteint d’une affection connue,
+      diagnostiquée avant l’admission, hospitalisé pour le traitement de celle-ci.
+      - Suveillance : La situation est celle d’un patient atteint d'une affection connue, antérieurement diagnostiquée,
+      éventuellement traitée (antérieurement traitée ou en cours de traitement), hospitalisé pour la surveillance de celle-ci.
+    """
+
+    text: str = dspy.InputField(desc="une synthèse de la situation.")
+    classification: Literal["Diagnostic", "Traitement", "Surveillance"] = (
+        dspy.OutputField()
+    )
     confidence: float = dspy.OutputField()
+
 
 class Extract(dspy.Module):
     def __init__(self, module):
@@ -43,9 +58,10 @@ class Extract(dspy.Module):
         response = self.module(text=text)
         return response.entities
 
+
 class ClinicalSituation(dspy.Module):
     def __init__(self):
-      self.module = dspy.ChainOfThought(Classify)
+        self.module = dspy.ChainOfThought(Classify)
 
     def forward(self, text):
         response = self.module(text=text)
