91880a9730acd08c22825f655406a7573c66ac05
Basile <basile.fuchs@chu-brest.fr>
2025-07-28 14:28:20 +0200

    feat(modules): ehance modules

--- clinical_situation/modules/__init__.py
+++ clinical_situation/modules/__init__.py
@@ -2,11 +2,28 @@ import dspy
 
 
 class ExtractPrimaryConditions(dspy.Signature):
-    """Extract structured primary conditions from text."""
+    """Extrait le motif d'hospitalisation à partir d'un compte rendu médical."""
 
-    text: str = dspy.InputField()
-    # title: str = dspy.OutputField()
-    headings: list[str] = dspy.OutputField()
-    entities: list[dict[str, str]] = dspy.OutputField(
-        desc="a list of primary conditions of admission"
-    )
+    text: str = dspy.InputField(desc="un compte rendu médical")
+    entities: list[str] = dspy.OutputField(desc="le motif d'hospitalisation.")
+
+
+class ExtractCares(dspy.Signature):
+    """Extrait tous les soins d'un compte rendu médicale."""
+
+    text: str = dspy.InputField(desc="un compte rendu médical")
+    entities: list[str] = dspy.OutputField(desc="une liste de tous les soins.")
+
+
+class ClinicalSituation(dspy.Module):
+    def __init__(self, module):
+        if module == "motif":
+            self.module = dspy.ChainOfThought(ExtractPrimaryConditions)
+        elif module == "soins":
+            self.module = dspy.ChainOfThought(ExtractCares)
+        else:
+            raise Exception("Ce module n'existe pas.")
+
+    def forward(self, text):
+        response = self.module(text=text)
+        return response.entities
