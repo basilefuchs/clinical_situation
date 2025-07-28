1d31cfd646dc3e64ad0439f0018d86f06ce5bfd5
Basile <basile.fuchs@chu-brest.fr>
2025-07-28 18:55:07 +0200

    feat(modules): add new

--- clinical_situation/modules/__init__.py
+++ clinical_situation/modules/__init__.py
@@ -1,27 +1,34 @@
 import dspy
+from typing import Literal
 
 
 class ExtractPrimaryConditions(dspy.Signature):
-    """Extrait le motif d'hospitalisation à partir d'un compte rendu médical."""
+    """Extraire le motif d'hospitalisation à partir d'un compte rendu médical."""
 
     text: str = dspy.InputField(desc="un compte rendu médical.")
     entities: list[str] = dspy.OutputField(desc="le motif d'hospitalisation.")
 
 
 class ExtractDiagnosis(dspy.Signature):
-    """Extrait la pathologie principale d'un compte rendu médical."""
+    """Extraire la pathologie principale d'un compte rendu médical."""
 
     text: str = dspy.InputField(desc="un compte rendu médical.")
     entities: list[str] = dspy.OutputField(desc="la pathologie principale.")
 
 class ExtractCares(dspy.Signature):
-    """Extrait tous les soins d'un compte rendu médicale."""
+    """Extraire tous les soins d'un compte rendu médicale."""
 
     text: str = dspy.InputField(desc="un compte rendu médical.")
     entities: list[str] = dspy.OutputField(desc="une liste de tous les soins.")
 
+class Classify(dspy.Signature):
+    """Classifier la situation clinique d'une hopitalisation à partir d'un compte rendu médical."""
 
-class ClinicalSituation(dspy.Module):
+    text: str = dspy.InputField(desc="un compte rendu médical.")
+    classification: Literal["Diagnostic", "Traitement", "Surveillance"] = dspy.OutputField()
+    confidence: float = dspy.OutputField()
+
+class Extract(dspy.Module):
     def __init__(self, module):
         if module == "motif":
             self.module = dspy.ChainOfThought(ExtractPrimaryConditions)
@@ -35,3 +42,11 @@ class ClinicalSituation(dspy.Module):
     def forward(self, text):
         response = self.module(text=text)
         return response.entities
+
+class ClinicalSituation(dspy.Module):
+    def __init__(self):
+      self.module = dspy.ChainOfThought(Classify)
+
+    def forward(self, text):
+        response = self.module(text=text)
+        return [response.classification, response.confidence]
