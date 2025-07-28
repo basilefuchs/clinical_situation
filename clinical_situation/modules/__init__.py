a35fdf3ce290b182616cb3b3e7e714316a29d899
Basile <basile.fuchs@chu-brest.fr>
2025-07-28 14:49:37 +0200

    fix(modules): seems to like dot !

--- clinical_situation/modules/__init__.py
+++ clinical_situation/modules/__init__.py
@@ -4,15 +4,15 @@ import dspy
 class ExtractPrimaryConditions(dspy.Signature):
     """Extrait le motif d'hospitalisation à partir d'un compte rendu médical."""
 
-    text: str = dspy.InputField(desc="un compte rendu médical")
-    entities: list[str] = dspy.OutputField(desc="le motif d'hospitalisation")
+    text: str = dspy.InputField(desc="un compte rendu médical.")
+    entities: list[str] = dspy.OutputField(desc="le motif d'hospitalisation.")
 
 
 class ExtractCares(dspy.Signature):
     """Extrait tous les soins d'un compte rendu médicale."""
 
-    text: str = dspy.InputField(desc="un compte rendu médical")
-    entities: list[str] = dspy.OutputField(desc="une liste de tous les soins")
+    text: str = dspy.InputField(desc="un compte rendu médical.")
+    entities: list[str] = dspy.OutputField(desc="une liste de tous les soins.")
 
 
 class ClinicalSituation(dspy.Module):
