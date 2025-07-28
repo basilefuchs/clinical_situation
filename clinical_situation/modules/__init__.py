207cda86224f2ab4fcf50b76eb3583c03e832b53
Basile <basile.fuchs@chu-brest.fr>
2025-07-28 14:37:02 +0200

    fix(modules): clean prompts

--- clinical_situation/modules/__init__.py
+++ clinical_situation/modules/__init__.py
@@ -5,14 +5,14 @@ class ExtractPrimaryConditions(dspy.Signature):
     """Extrait le motif d'hospitalisation à partir d'un compte rendu médical."""
 
     text: str = dspy.InputField(desc="un compte rendu médical")
-    entities: list[str] = dspy.OutputField(desc="le motif d'hospitalisation.")
+    entities: list[str] = dspy.OutputField(desc="le motif d'hospitalisation")
 
 
 class ExtractCares(dspy.Signature):
     """Extrait tous les soins d'un compte rendu médicale."""
 
     text: str = dspy.InputField(desc="un compte rendu médical")
-    entities: list[str] = dspy.OutputField(desc="une liste de tous les soins.")
+    entities: list[str] = dspy.OutputField(desc="une liste de tous les soins")
 
 
 class ClinicalSituation(dspy.Module):
