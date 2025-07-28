34dc90b857bfb3e3710feb2551a33d80cc5e1cbd
Basile <basile.fuchs@chu-brest.fr>
2025-07-28 14:32:59 +0200

    feat(main): ehance

--- clinical_situation/__main__.py
+++ clinical_situation/__main__.py
@@ -1,25 +1,54 @@
 import yaml
 import dspy
-from config import load_config, lm
-from modules import ExtractPrimaryConditions
+from config import lm
+from modules import ExtractPrimaryConditions, ExtractCares, ClinicalSituation
 
-
-# config = load_config("clinical_situation/config/config.yaml")
-
-# lm = dspy.LM(config["llm"]["model"], api_base=config["llm"]["port"], api_key="")
 dspy.configure(lm=lm())
 
-text = (
-    """
-    Patient hospitalisé pour prise en charge d'une appendicectomie 
-    devant une douleur abdominale et un météorisme.Notion de chute à domicile.
-    Conclusion : Chute de sa hauteur dans un contexte démence non étiquettée, 
-    compliquée d'une station allongée responsable d'une appendicite. 
-    Sortie en SSR.
-    """
-)
+text = """
+Compte Rendu d'Hospitalisation
 
-modulePC = dspy.Predict(ExtractPrimaryConditions)
-responsePC = modulePC(text=text)
+Identité du patient :
 
-print(responsePC.headings)
+Nom : Jeanne
+Prénom : Dupont
+Âge : 32 ans
+Sexe : Féminin
+Mode d'entrée : Surveillance à haut risque
+
+Durée de l'hospitalisation : 3 jours
+
+Service : Obstétrique
+
+Motif d'admission :
+Jeanne Dupont, une femme de 32 ans, a été admise à la maternité en raison de douleurs abdominales persistantes et d'une cervix prématurément dilatée. En raison de son état critique, elle nécessitait une surveillance à haut risque pour éviter un accouchement prématuré.
+
+Antécédents médicaux :
+Jeanne est enceinte de jumeaux et présente déjà un passé médical complexe marqué par plusieurs grossesses à haut risque précédentes. Aucun autre antécédent médical connu.
+
+Examen clinique à l'admission :
+À son arrivée, Jeanne présentait une cervix dilatée de 3 cm et des contractions abdominales fréquentes. L'échographie a révélé que les deux bébés étaient en bonne position et avaient un poids acceptable.
+
+Résultats biologiques :
+
+FHC : 0.5 ng/mL (valeur normale < 0.1 ng/mL)
+Glycémie : 90 mg/dL (valeur normale < 140 mg/dL)
+Hémoglobine A1c : 5.2% (valeur normale < 5.7%)
+Créatinine : 0.8 mg/dL (valeur normale < 1.3 mg/dL)
+Cholestérol total : 180 mg/dL (valeur normale < 200 mg/dL)
+LDL : 110 mg/dL (valeur normale < 100 mg/dL)
+HDL : 60 mg/dL (valeur normale > 40 mg/dL)
+Triglycérides : 150 mg/dL (valeur normale < 150 mg/dL)
+Pathologies associées fictives :
+Jeanne présente également une insuffisance rénale chronique légère et un hypercholestérolémie, mais il n'y a pas de complications majeures à ce jour.
+
+Traitement et évolution :
+Jeanne a reçu un traitement pour arrêter les contractions abdominales, ainsi qu'un suivi constant de son cervix et des bébés. Elle a également été administrée des hormones progestéroneiques pour retarder l'accouchement prématuré. Au fil des jours, sa cervix s'est rétrécie progressivement et les contractions abdominales ont diminué.
+
+Conclusion et recommandations :
+Après une hospitalisation de 3 jours, Jeanne a pu quitter l'hôpital avec un diagnostic final d'un faux travail avant 37 semaines entières de gestation. Pour assurer un suivi optimal des jumeaux, il est recommandé de surveiller régulièrement la croissance intra-utérine et la fonction cardiaque de Jeanne, ainsi que de suivre de près l'évolution de son insuffisance rénale chronique légère. Des visites de suivi régulières sont également nécessaires pour s'assurer qu'il n'y a pas de récidive du faux travail avant l'accouchement prévu à terme.
+"""
+
+extract = ClinicalSituation("soins")
+
+print(extract(text))
